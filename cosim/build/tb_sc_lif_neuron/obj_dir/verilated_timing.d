01ea5ffdf30abebbf322e8292948c1f145002523
Miroslav Šotek <35185786+anulum@users.noreply.github.com>
2026-02-10 14:03:10 +0100

    Delete cosim/build directory

--- cosim/build/tb_sc_lif_neuron/obj_dir/verilated_timing.d
+++
@@ -1,8 +0,0 @@
-verilated_timing.o: \
- C:\Users\forti\OneDrive\Documents\aaa_God_of_the_Math_Collection\03_CODE\sc-neurocore\.venv\Lib\site-packages\verilator/include/verilated_timing.cpp \
- C:\Users\forti\OneDrive\Documents\aaa_God_of_the_Math_Collection\03_CODE\sc-neurocore\.venv\Lib\site-packages\verilator/include/verilated_timing.h \
- C:\Users\forti\OneDrive\Documents\aaa_God_of_the_Math_Collection\03_CODE\sc-neurocore\.venv\Lib\site-packages\verilator/include/verilated.h \
- C:\Users\forti\OneDrive\Documents\aaa_God_of_the_Math_Collection\03_CODE\sc-neurocore\.venv\Lib\site-packages\verilator/include/verilated_config.h \
- C:\Users\forti\OneDrive\Documents\aaa_God_of_the_Math_Collection\03_CODE\sc-neurocore\.venv\Lib\site-packages\verilator/include/verilatedos.h \
- C:\Users\forti\OneDrive\Documents\aaa_God_of_the_Math_Collection\03_CODE\sc-neurocore\.venv\Lib\site-packages\verilator/include/verilated_types.h \
- C:\Users\forti\OneDrive\Documents\aaa_God_of_the_Math_Collection\03_CODE\sc-neurocore\.venv\Lib\site-packages\verilator/include/verilated_funcs.h
